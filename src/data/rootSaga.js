f15074599210dd129db8f439ab34c15e5301f46b
kim-taewoo <acoustic0419@gmail.com>
2020-10-05 01:30:25 +0900

    rootSaga 에서 recoverable 제너레이터를 사용해서 에러 상황 발생시 앱 전체가 중지되지 않도록 함

--- src/data/rootSaga.js
+++ src/data/rootSaga.js
@@ -1,22 +1,21 @@
-import { all, fork } from 'redux-saga/effects';
-import user from './user/sagas';
-import posts from './posts/sagas';
-import comments from './comments/sagas';
-import loading from './loading/sagas';
+import { call, all } from 'redux-saga/effects';
+import postSaga from './posts/sagas';
+import commentSaga from './comments/sagas';
+import userSaga from './user/sagas';
 
-// function* recoverable(...args) {
-//   while (true) {
-//     try {
-//       yield call(...args);
-//     } catch (e) {
-//       Sentry.captureException(e);
-//       if (process.env.NODE_ENV !== 'production') {
-//         console.error(e);
-//       }
-//     }
-//   }
-// }
+function* recoverable(...args) {
+  while (true) {
+    try {
+      yield call(...args);
+    } catch (e) {
+      if (process.env.NODE_ENV !== 'production') {
+        console.error(e);
+      }
+      // 필요시 센트리 에러 로그 적재
+    }
+  }
+}
 
 export default function* rootSaga() {
-  yield all([fork(user), fork(posts), fork(comments), fork(loading)]);
+  yield all([recoverable(postSaga), recoverable(commentSaga), recoverable(userSaga)]);
 }
